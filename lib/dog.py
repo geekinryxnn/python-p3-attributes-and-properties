e25a07330ad8437521a08da3fe221e488e3379be
geekinryxnn <ryankiratu67@gmail.com>
2025-05-23 11:15:35 +0300

    new code

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,41 @@
 #!/usr/bin/env python3
 
-APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
-]
+
+# lib/dog.py
 
 class Dog:
-    pass
+    approved_breeds = ["Mastiff", "Chihuahua", "Corgi", "Shar Pei", "Beagle", "French Bulldog", "Pug", "Pointer"]
+
+    def __init__(self, name="Unknown", breed="Mastiff"):
+        self.name = name  # Use the property setter
+        self.breed = breed  # Use the property setter
+
+    def get_name(self):
+        return getattr(self, "_name", None)
+
+    def set_name(self, name):
+        if type(name) is str and 1 <= len(name) <= 25:
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    def get_breed(self):
+        return getattr(self, "_breed", None)
+
+    def set_breed(self, breed):
+        if breed in self.approved_breeds:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    breed = property(get_breed, set_breed)
+
+if __name__ == '__main__':
+    my_dog = Dog(name="Buddy", breed="Beagle")
+    print(f"Dog's name: {my_dog.name}")
+    print(f"Dog's breed: {my_dog.breed}")
+
+    my_dog.name = "Super Long Name That Exceeds Twenty-Five Characters"
+    my_dog.breed = "Alien"
--- lib/person.py
+++ lib/person.py
@@ -1,19 +1,42 @@
 #!/usr/bin/env python3
 
-APPROVED_JOBS = [
-    "Admin",
-    "Customer Service",
-    "Human Resources",
-    "ITC",
-    "Production",
-    "Legal",
-    "Finance",
-    "Sales",
-    "General Management",
-    "Research & Development",
-    "Marketing",
-    "Purchasing"
-]
+# lib/person.py
 
 class Person:
-    pass
+    approved_jobs = ["Admin", "Customer Service", "Human Resources", "ITC", "Production", "Legal", "Finance", "Sales", "General Management", "Research & Development", "Marketing", "Purchasing"]
+
+    def __init__(self, name="Unknown", job="Unemployed"):
+        self.name = name
+        self.job = job
+
+    # --- Name property ---
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if type(name) is str and 1 <= len(name) <= 25:
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    # --- Job property ---
+    def get_job(self):
+        return self._job
+
+    def set_job(self, job):
+        if job in self.approved_jobs:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    job = property(get_job, set_job)
+
+if __name__ == '__main__':
+    person1 = Person(name="john doe", job="Sales")
+    print(f"Person's name: {person1.name}")
+    print(f"Person's job: {person1.job}")
+
+    person1.name = ""
+    person1.job = "Astronaut"
